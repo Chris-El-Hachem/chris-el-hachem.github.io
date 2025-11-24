8eb2a5d689e2a7885febb5de1004600c946f7ea9
Chris-E <elhachemchris73@gmail.com>
2025-11-25 01:54:15 +1100

    setting up script.js to be huuuge
    for now, i will ignore fetch() and see how far i go if i try cramming all of the page contents into a json into the script.js file.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,19 @@
+const messages = {
+"home": `
+<h1>Hi, I'm Chris!</h1>
+<p>I am a third-year Mechanical and Mechatronic Engineering student.</p>
+`,
+
+"contact": `
+<h1>Contact me</h1>
+<p>linkedin:</p>
+`,
+}
+
 const cont = document.getElementById("main-content");
-cont.innerHTML = "<p>pleeeeeease<\p>"
-fetch('/home.html')
-.then(res=>res.text())
-.then(data=>{
-    cont.innerHTML = data
-})
+
+async function goToPage(pageName) {
+    cont.innerHTML = messages[pageName];
+}
+
+goToPage("home");
