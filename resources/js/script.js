071eb847145d09996c5c8b468b922f2255b8606e
Chris-E <elhachemchris73@gmail.com>
2025-11-26 00:55:00 +1100

    one banner
    hopefully a banner to come for each page

--- resources/js/script.js
+++ resources/js/script.js
@@ -22,7 +22,7 @@ const pageContents = {
 
 "contact": `
 <h1>Contact Me</h1>
-<p>Feel free to reach out to me about anything on the following platforms:</p>
+<p>If you want to reach out to me about anything, or want to see what I'm up to, feel free to do so on the following platforms:</p>
 <p>Email: elhachemchris73@gmail.com</p>
 <p>Linkedin:
     <a
@@ -33,13 +33,47 @@ const pageContents = {
         linkedin.com/in/chris-el-hachem
     <\a>
 </p>
+<p>GitHub:
+    <a
+        href="https://github.com/Chris-El-Hachem"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        github.com/Chris-El-Hachem
+    <\a>
+</p>
+`,
+}
+
+const banners = {
+"home": `
+<img>
+`,
+
+"all-projects": `
+<img>
+`,
+
+"skills": `
+<img>
+`,
+
+"about": `
+<img>
+`,
+
+"contact": `
+<img src="resources/icons/network_banner.jpg" class="img-fluid" alt="...">
 `,
 }
 
 const cont = document.getElementById("main-content");
+const banner_div = document.getElementById("banner")
 
 async function goToPage(pageName) {
     cont.innerHTML = pageContents[pageName];
+    banner_div.innerHTML = banners[pageName];
+
 }
 
 goToPage("home");
