a406c3fcc32bd7613e820fdfcac2babd2e9270ec
Chris-E <elhachemchris73@gmail.com>
2025-11-25 17:47:34 +1100

    footer

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,45 @@
-const messages = {
+const pageContents = {
 "home": `
 <h1>Hi, I'm Chris!</h1>
+<p>Thanks for checking out my portfolio!</p>
 <p>I am a third-year Mechanical and Mechatronic Engineering student.</p>
 `,
 
+"all-projects": `
+<h1>All Projects</h1>
+<p>osteotomy</p>
+`,
+
+"skills": `
+<h1>Skills</h1>
+<p>website</p>
+`,
+
+"about": `
+<h1>About Me</h1>
+<p>education</p>
+`,
+
 "contact": `
-<h1>Contact me</h1>
-<p>linkedin:</p>
+<h1>Contact Me</h1>
+<p>Feel free to reach out to me about anything on the following platforms:</p>
+<p>Email: elhachemchris73@gmail.com</p>
+<p>Linkedin:
+    <a
+        href="https://www.linkedin.com/in/chris-el-hachem/"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        linkedin.com/in/chris-el-hachem
+    <\a>
+</p>
 `,
 }
 
 const cont = document.getElementById("main-content");
 
 async function goToPage(pageName) {
-    cont.innerHTML = messages[pageName];
+    cont.innerHTML = pageContents[pageName];
 }
 
 goToPage("home");
